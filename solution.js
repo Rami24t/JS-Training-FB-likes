6ccd4fe08c26111f8e6b5190cf71bbd64e40d757
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-21 17:49:36 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,3 +1,12 @@
+// In the "like" system from pages like facebook people can "like" posts, pictures or other items.
+// Your function should create the text that should be displayed next to such an item.
+// Implement the function 'likes' which takes an array containing the names of people that like an item
+// and returns the display text as shown in these examples:
+// [] --> "no one likes this" ["Botros"] --> "Botros likes this" ["Gallops", "Alex"] --> "Gallops and Alex like this" ["Maxi", "Johanna", "Markus"] --> "Maxi, Johanna and Markus like this" ["Ella", "Bob", "Mark", "Max"] --> "Ella, Bob and 2 others like this"
+// ['Rami', 'Jacob', 'Mark', 'Max','Kristie','Christie','Lewak','Brita','Erico', 'Dani']
+// --> 'Rami, Jacob and 8 others like this'
+
+
 function likes(names) {
   return names ? (names.length==1 ? names[0]+ " likes this" : (names.length ==2 ? names.join(' and ') + 
   ' like this' : (names.length==3? names[0] + ', ' + names[1] + ' and '+names[2] +' like this' : names.length>3 ? names[0]+', '+
